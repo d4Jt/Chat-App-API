0274d3bfb194bad2d32d9495a85ad027aacc6ba3
d4Jt <jsmsj04@gmail.com>
2024-02-28 17:10:04 +0700

    Write API users, chats, message

---
+++ src/routes/index.route.js
@@ -0,0 +1,10 @@
+const routes = (app) => {
+   app.use('/api/users', require('./user.route'));
+   app.use('/api/chats', require('./chat.route'));
+   app.use('/api/messages', require('./message.route'));
+   app.get('/', (req, res) => {
+      res.send('Hello World!');
+   });
+};
+
+module.exports = routes;
